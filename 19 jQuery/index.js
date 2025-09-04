071d6e1522a2e0f14c6606915e5d98dd3e83b09c
manas09rai@gmail.com <manas09rai@gmail.com>
2025-09-04 14:38:17 -0500

    Added introductory lesson to incorporate jquery to website

---
+++ 19 jQuery/index.js
@@ -0,0 +1,5 @@
+$("h1").css("color","red"); // Use this method if you want to place the script tages at the end of <body>
+
+// $(document).ready(function(){ // Use this method if you want to place the script tags in the <head> section instead of at the end of <body>
+//     $("h1").css("color","red");
+// })
