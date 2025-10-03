1f96517046d7c648027ced89a6447c1e73c5bb61
Manasraimovieguy <manas09rai@gmail.com>
2025-10-03 11:38:29 -0500

    You mind tellin me what a file system is?

---
+++ 23 Node/23.2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+// fs.writeFile("message.txt","Woah!!!!!!! Just added this line again",(err)=>{
+//     if(err) throw err;
+//     console.log("Great scott!!! The file has been saved");
+// })
+
+fs.readFile("message.txt","utf-8",(err,data)=>{ // IMP: Order of parameters matter here, I put (data,err) and was getting an error thrown and didn't know why
+    if (err) throw err;
+    console.log(data);
+})
