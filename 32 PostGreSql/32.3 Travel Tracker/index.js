de06276eba4505a2c4fb3eac1098f39fd5515490
Manasraimovieguy <manas09rai@gmail.com>
2026-08-12 08:46:52 -0500

    Travel Tracker Exercise 2

--- 32 PostGreSql/32.3 Travel Tracker/index.js
+++ 32 PostGreSql/32.3 Travel Tracker/index.js
@@ -29,10 +29,39 @@ app.get("/", async (req, res) => {
   for(let i=0;i<result.rows.length;i++){ //stupid way
     countries.push(result.rows[i].country_code)
   }
-  console.log(countries);
+  // console.log(countries);
   res.render("index.ejs",{countries: countries, total: countries.length});
-  db.end();
+  // db.end();
 });
+
+//first attempt
+// app.post("/add", async (req, res) => {
+//   const country = req.body.country;
+//   console.log(country);
+//   const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [country]);
+//   // console.log(result);
+//   if(result.rows){
+//     await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [result.rows[0].country_code]);
+//     const result1 = await db.query("SELECT country_code FROM visited_countries");
+//     let countries = [];
+//     result1.rows.forEach((country)=>{
+//       countries.push(country.country_code);
+//     });
+//     res.render("index.ejs",{countries: countries, total: countries.length});
+//   }
+// })
+
+app.post("/add", async (req, res) => {
+  const country = req.body.country;
+  console.log(country);
+  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [country]);
+  // console.log(result);
+  if(result.rows){
+    await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [result.rows[0].country_code]);
+    res.redirect("/"); // just call the get method, really cool way, you can also call a repeatable function
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
