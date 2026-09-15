5b1ebf52d0cd2428aa4d8d9c450438a9268bcbd6
Manasraimovieguy <manas09rai@gmail.com>
2026-09-15 07:49:30 -0500

    Permalist project, I am permanently done with this module baybeeeeeee

--- 32 PostGreSql/32.5 Permalist Project/index.js
+++ 32 PostGreSql/32.5 Permalist Project/index.js
@@ -1,33 +1,56 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "Daigo@Dojima#0",
+  port: 5432,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+const result = await db.query("Select * from items");
+// console.log(result.rows);
+let items = result.rows;
 
-app.get("/", (req, res) => {
+async function getLatestList(){
+  const latestList = await db.query("Select * from items");
+  return latestList.rows;
+}
+
+app.get("/", async (req, res) => {
+  items = await getLatestList();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  await db.query("insert into items (title) values ($1)", [item]);
+  // items.push({ title: item });
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  await db.query("Update items set title=($1) where id=($2)",[req.body.updatedItemTitle, req.body.updatedItemId]);
+  res.redirect("/");
 
-app.post("/delete", (req, res) => {});
+});
+
+app.post("/delete", async (req, res) => {
+  await db.query("Delete from items where id=($1)",[req.body.deleteItemId]);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
